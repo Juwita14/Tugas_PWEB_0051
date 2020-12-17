29ef966a4314ac0cd2ae263dab4382b4c758b997
Juwita14 <56763570+Juwita14@users.noreply.github.com>
2020-12-17 05:16:32 -0800

    Update login.php

--- login.php
+++ login.php
@@ -1,17 +1,16 @@
-<?php session_start();
+<?php
+session_start();
 include "koneksi.php";
 $username = $_POST['username'];
-$password = md5($_POST['password']);
+$password = $_POST['password'];
 $query = mysqli_query($koneksi, "select * from user where username='$username' and password='$password'");
 $cek = mysqli_num_rows($query);
 
 if ($cek) {
     $_SESSION['username']=$username;
     ?><script language="javascript"> window.location="upload.php";</script><?php
-    
 }
 else {
     ?><script language="javascript"> window.location="index.php";</script><?php
-    
 }
 ?>
